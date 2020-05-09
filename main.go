3cbdb023ad026ff9151241867fa4188f992a0be8
kenzo0107 <kenzo.tanaka0107@gmail.com>
2020-05-09 21:58:49 +0900

    Separately Downloader in go
    It downloads files separately and binds the files to one file when it specifies the target url in parallel.

--- main.go
+++ main.go
@@ -1,100 +1,154 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strconv"
-	"sync"
 
 	"github.com/pkg/errors"
-	"gopkg.in/cheggaaa/pb.v1"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
 	procs     int    = runtime.NumCPU()
 	body             = make([]string, procs+1)
-	targetURL string = "https://ubuntu.com/download/desktop/thank-you"
-	filename  string = "hoge"
+	targetURL string = "http://kenzo0107.github.io/"
+	filename  string = "index.html"
+	dst       string = "tmp"
 )
 
-func filesize() int {
-	res, _ := http.Head(targetURL)
-	maps := res.Header
-	length, _ := strconv.Atoi(maps["Content-Length"][0])
-	return length
+func main() {
+	if err := download(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func main() {
-	download()
+func _main() {
+
 }
 
 func download() error {
-	filesize := filesize()
+	// ダウンロードする URL からファイルサイズ取得
+	filesize, err := filesize(targetURL)
+	if err != nil {
+		return err
+	}
+
+	// ファイルサイズを並行処理数で割ったサイズ
 	chunk := filesize / procs
+
+	// ファイルサイズを並行処理数で割った余り
 	lastRequestSize := filesize % procs
 
 	// goroutine で並行して range access でダウンロード
-	wg := new(sync.WaitGroup)
+	bc := context.Background()
+	eg, ctx := errgroup.WithContext(bc)
 	for i := 0; i < procs; i++ {
-		wg.Add(1)
+		i := i
+		// bytes=<min>-<max>
 		min := chunk * i
 		max := chunk * (i + 1)
 
 		if i == procs-1 {
 			max += lastRequestSize
 		}
-
-		go rangeAccessRequest(wg, min, max, i, targetURL)
+		eg.Go(func() error {
+			select {
+			case <-ctx.Done():
+				fmt.Println("canceled")
+				return nil
+			default:
+				return rangeAccessRequest(min, max, i, targetURL)
+			}
+		})
 	}
-	wg.Wait()
 
-	return bindwithFiles(filename, filesize)
+	// eg.Go() でエラーで一番最初のエラーを返す
+	if err := eg.Wait(); err != nil {
+		return err
+	}
+
+	// Range Access でダウンロードした分割ファイルをつなぎ合わせる
+	if err := bindwithFiles(filename, filesize); err != nil {
+		return err
+	}
+	return nil
 }
 
-func rangeAccessRequest(wg *sync.WaitGroup, min, max, i int, url string) {
-	defer wg.Done()
+func filesize(targetURL string) (int, error) {
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		return 0, err
+	}
+	res, err := http.Head(u.String())
+	if err != nil {
+		return 0, err
+	}
+	maps := res.Header
+	length, _ := strconv.Atoi(maps["Content-Length"][0])
+	return length, nil
+}
 
+// Range Access ダウンロード
+func rangeAccessRequest(min, max, i int, url string) error {
 	req, _ := http.NewRequest("GET", url, nil)
 	rangeBytes := fmt.Sprintf("bytes=%d-%d", min, max)
 	req.Header.Add("Range", rangeBytes)
 
 	httpClient := new(http.Client)
-	resp, errRes := httpClient.Do(req)
-	if errRes != nil {
-		log.Fatal(errRes)
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
 	}
+	defer func() {
+		err = resp.Body.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
 
-	defer resp.Body.Close()
-	reader, _ := ioutil.ReadAll(resp.Body)
+	reader, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
 	body[i] = string(reader)
-	ioutil.WriteFile(strconv.Itoa(i), []byte(string(body[i])), 0x777)
+	fpath := filepath.Join(dst, strconv.Itoa(i))
+	if err := ioutil.WriteFile(fpath, []byte(string(body[i])), 0x777); err != nil {
+		return err
+	}
+	return nil
 }
 
+// Ragen Access 分割ダウンロードしたファイルを1つにまとめる
 func bindwithFiles(filename string, filesize int) error {
 	fh, err := os.Create(filename)
 	if err != nil {
 		return errors.Wrap(err, "failed to create a file in download location")
 	}
-	defer fh.Close()
-
-	bar := pb.New64(int64(filesize))
-	bar.Start()
+	defer func() {
+		err = fh.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
 
 	var f string
 	for i := 0; i < procs; i++ {
-		f = strconv.Itoa(i)
+		i := i
+		f = filepath.Join(dst, strconv.Itoa(i))
 		subfp, err := os.Open(f)
 		if err != nil {
 			return errors.Wrap(err, "failed to open "+f+" in download location")
 		}
-
-		proxy := bar.NewProxyReader(subfp)
-		io.Copy(fh, proxy)
+		io.Copy(fh, subfp)
 
 		// Not use defer
 		subfp.Close()
